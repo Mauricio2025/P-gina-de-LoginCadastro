eba554a5a04f1bf84d414de4398a9999f08d9d3a
Mauricio Souza da Silva <mauriciiosouza51@gmail.com>
2023-06-01 14:27:59 -0300

    atualização

---
+++ index.js
@@ -0,0 +1,5 @@
+const logregBox = document.querySelector('logreg-box');
+const loginLinK = document.querySelector('login-link');
+const registerLink = document.querySelector('register-link');
+
+registerLink.addEventListener('click', () => {logregBox.classList.add('active');})
